dc248f164a249852dfd55a24c90a3af02f217d08
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-06-30 19:39:00 +0800

    update 1.two-sum.java

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -14,21 +14,21 @@
  * <p>Given an array of integers <code>nums</code>&nbsp;and an integer
  * <code>target</code>, return <em>indices of the two numbers such that they
  * add up to <code>target</code></em>.</p>
- * 
+ *
  * <p>You may assume that each input would have <strong><em>exactly</em> one
  * solution</strong>, and you may not use the <em>same</em> element twice.</p>
- * 
+ *
  * <p>You can return the answer in any order.</p>
- * 
+ *
  * <p>&nbsp;</p>
  * <p><strong>Example 1:</strong></p>
- * 
+ *
  * <pre>
  * <strong>Input:</strong> nums = [2,7,11,15], target = 9
  * <strong>Output:</strong> [0,1]
  * <strong>Output:</strong> Because nums[0] + nums[1] == 9, we return [0, 1].
  * </pre>
- * 
+ *
  * <p><strong>Example 2:</strong></p>
  * 
  * <pre>
@@ -57,20 +57,17 @@
  * <strong>Follow-up:&nbsp;</strong>Can you come up with an algorithm that is
  * less than&nbsp;<code>O(n<sup>2</sup>)&nbsp;</code>time complexity?
  */
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map <Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i){
-                return new int[] {i, map.get(complement)};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]) {
+                    return new int[] { i, j };
+                }             
             }
         }
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("No Two Sum Solutions.");
     }
 }
 
