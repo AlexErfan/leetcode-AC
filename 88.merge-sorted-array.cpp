87ed14a9d75539ee3df404d488779feb0391266f
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-08-02 22:22:44 +0800

    update 88.merge-sorted-array.cpp

--- 88.merge-sorted-array.cpp
+++ 88.merge-sorted-array.cpp
@@ -72,23 +72,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k = m + n;
-        while (m > 0 && n > 0) {
-           if (nums1[m-1] > nums2[n-1]) {
-                nums1[k-1] = nums1[m-1];
-                --m;
-           } else {
-                nums1[k-1] = nums2[n-1];
-                --n;
-           }
-           --k;
+        for (int i = m; i < m+n; i++) {
+            nums1[i] = nums2[i-m];
         }
-        
-        // handle situation when m=0, n>0
-        while (n > 0) {
-           nums1[k-1]  = nums2[n-1];
-           --k;
-           --n;
-        }
+
+        sort(nums1.begin(), nums1.end());
     }
 };
