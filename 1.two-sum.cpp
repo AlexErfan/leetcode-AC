7594899d6ca08cab307d930962387d9b39b19196
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-09-10 14:29:03 -0500

    update 1.two-sum.cpp

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -60,14 +60,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if (map.count(complement))
-                return {i, map[complement]};
-            map[nums[i]] = i;
+            int remain = target - nums[i];
+            for (int j = 0; j < nums.size(); j++) {
+                if (remain == nums[j] && j!=i) {
+                    return {i, j} ;
+                } 
+            }
         }
+
         return {};
 }
 
