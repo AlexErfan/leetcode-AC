5b5711413e378281bf120d49ed8495ead86ba81a
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-09-24 17:51:44 -0500

    update 119.pascals-triangle-ii.cpp

---
+++ 119.pascals-triangle-ii.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=119 lang=cpp
+ *
+ * [119] Pascal's Triangle II
+ *
+ * https://leetcode.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (54.43%)
+ * Total Accepted:    418.1K
+ * Total Submissions: 767.8K
+ * Testcase Example:  '3'
+ *
+ * Given an integer rowIndex, return the rowIndexth (0-indexed) row of the
+ * Pascal's triangle.
+ * 
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it as shown:
+ * 
+ * 
+ * Example 1:
+ * Input: rowIndex = 3
+ * Output: [1,3,3,1]
+ * Example 2:
+ * Input: rowIndex = 0
+ * Output: [1]
+ * Example 3:
+ * Input: rowIndex = 1
+ * Output: [1,1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= rowIndex <= 33
+ * 
+ * 
+ * 
+ * Follow up: Could you optimize your algorithm to use only O(rowIndex) extra
+ * space?
+ * 
+ */
+class Solution {
+private:
+public:
+    vector<int> getRow(int rowIndex) {
+        if (rowIndex == 0)
+            return {1};
+        if (rowIndex == 1)
+            return {1,1};
+
+        vector<int> cur;
+        vector<int> prev = getRow(rowIndex-1);
+
+        cur.push_back(1);
+        for (int i = 1; i < rowIndex; i++) {
+            cur.push_back(prev[i-1] + prev[i]);
+        }
+        cur.push_back(1);
+
+        return cur;
+    }
+};
