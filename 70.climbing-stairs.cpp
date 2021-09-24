4eb15cfb792b390a1e2723117ef26978609f00b3
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-09-24 12:20:04 -0500

    update 70.climbing-stairs.cpp

---
+++ 70.climbing-stairs.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=70 lang=cpp
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (49.51%)
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '2'
+ *
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * 
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 45
+ * 
+ * 
+ */
+class Solution {
+private:
+    int helper(int steps, int n, vector<int>& cache) {
+        if (steps == n)
+            return 1;
+        if (steps > n)
+            return 0;
+        if (cache[steps] > 0)
+            return cache[steps];
+        cache[steps] = helper(steps+1, n, cache) + helper(steps+2, n, cache);
+        //return helper(steps+1, n, cache) + helper(steps+2, n, cache); 
+        return cache[steps];
+    }
+
+    //int helper(int count, int n, vector<int>& cache) {
+    //    if (count == n)
+    //        return 1;
+    //    else if (count > n)
+    //        return 0;
+    //    if (cache[count > 0])
+    //        return cache[count];
+
+    //    cache[count] = helper(count+1, n, cache) + helper(count+2, n, cache);
+    //    return helper(count+1, n, cache) + helper(count+2, n, cache);
+
+    //}
+public:
+    int climbStairs(int n) {
+        vector<int> cache (n+1, 0);
+        return helper(0, n, cache);
+    }
+};
