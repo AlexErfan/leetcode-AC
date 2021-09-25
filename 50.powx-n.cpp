8417346d8084bdff61779648141f9443b35cbdaa
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-09-25 13:07:49 -0500

    update 50.powx-n.cpp

--- 50.powx-n.cpp
+++ 50.powx-n.cpp
@@ -51,14 +51,12 @@ private:
     double fastPow(double x, int n) {
         if (n == 0)
             return 1.0;
-
         double half = fastPow(x, n/2);
-        if (n % 2 == 0)
+        if (n % 2 ==0)
             return half*half;
         else
             return half*half*x;
     }
-
 public:
     double myPow(double x, int n) {
         long long N = n;
@@ -66,8 +64,8 @@ public:
             N = -N;
             x = 1/x;
         }
-
         return fastPow(x, N);
+
     }
 };
 
