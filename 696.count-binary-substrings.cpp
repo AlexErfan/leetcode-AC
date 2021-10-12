c37bda4da6d93447cb6a0893af05d74d4f4ef35b
Alex WANG <32976913+AlexErfan@users.noreply.github.com>
2021-10-12 11:00:24 -0500

    update 696.count-binary-substrings.cpp

--- 696.count-binary-substrings.cpp
+++ 696.count-binary-substrings.cpp
@@ -53,7 +53,7 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int ans=0, prev=0, cur=1;
+        int ans = 0, prev = 0, cur = 1;
 
         for (int i = 1; i < s.size(); i++) {
             if (s[i-1] != s[i]) {
